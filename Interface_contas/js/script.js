793a3abd67f244ce1fbbbb6607b6a929688543c6
JulianoBat <julianobatss@gmail.com>
2023-05-30 13:03:57 -0300

    Atualização de Interface (Falta correção do campo atraso no script)

--- Interface_contas/js/script.js
+++ Interface_contas/js/script.js
@@ -1,40 +1,48 @@
 const formEl = document.getElementById('form-api');
-const IdataVenc = document.querySelector(".dt-venc");
+const IdataVenc = document.querySelector("#dt-venc").value;
+const IstatusConta = document.querySelector("input[name='statusConta']:checked").value;
 
 
-
-function momentoAtual(){
+function momentoAtual() {
     let data = new Date(),
-        dia  = data.getDate().toString().padStart(2, '0'),
-        mes  = (data.getMonth()+1).toString().padStart(2, '0'),
-        ano  = data.getFullYear();
+        dia = data.getDate().toString().padStart(2, '0'),
+        mes = (data.getMonth() + 1).toString().padStart(2, '0'),
+        ano = data.getFullYear();
     return `${ano}-${mes}-${dia}`;
 };
 
-function verificaVenc(){
-    if (momentoAtual() <= IdataVenc) {
-        return 'false';
-    }
-    else {
-        return 'true';
-    }
-}
+
+
+
 
 formEl.addEventListener('submit', evento => {
     evento.preventDefault();
 
-    const momentoA = momentoAtual()
+    const momentoA = momentoAtual();
     const formData = new FormData(formEl);
-    const data = Object.fromEntries(formData)
+    const data = Object.fromEntries(formData);
     data.dtReg = momentoA;
     data.atraso = verificaVenc();
-    
+
+    function verificaVenc() {
+        if (momentoAtual() > IdataVenc) {
+            console.log(IdataVenc)
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    console.log(verificaVenc());
+
     fetch('http://localhost:8080/conta', {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json'
-    },
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(data)
     }).then(res => res.json()).then(data => console.log(data))
-})
+});
 
---
+++ Interface_contas/js/scriptTabela.js
@@ -0,0 +1,99 @@
+const url = "http://localhost:8080/conta"
+
+const tbody = document.querySelector("#tbody");
+
+async function getAll() {
+    const response = await fetch(url)
+    const data = await response.json();
+    
+    
+    
+    for(let i = 0; i < data.length; i++) {
+        const tr = document.createElement("tr");
+        const contaEmpresa = document.createElement("td");
+        const dtVenc = document.createElement("td");
+        const status = document.createElement("td");
+        const valor = document.createElement("td");
+
+        contaEmpresa.innerText = data[i].contaEmpresa;
+        dtVenc.innerText = data[i].dtVenc;
+        status.innerText = data[i].statusConta;
+        valor.innerText = data[i].valor;
+
+        tr.appendChild(contaEmpresa);
+        tr.appendChild(dtVenc);
+        tr.appendChild(status);
+        tr.appendChild(valor);
+
+        if(data[i].statusConta === 'PENDENTE') {
+            tr.setAttribute("class", "table-warning");
+        }
+
+        if(data[i].atraso === true){
+            tr.setAttribute("class", "table-danger");
+        }
+
+        if(data[i].statusConta === 'PAGO') {
+            tr.setAttribute("class", "table-success");
+        }
+
+        
+
+        tbody.appendChild(tr);
+    }
+    
+
+    
+
+
+    /*data.map((conta) => {
+        const tr = document.createElement("tr");
+        const contaEmpresa = document.createElement("td");
+        const dtVenc = document.createElement("td");
+        const status = document.createElement("td");
+        const valor = document.createElement("td");
+
+        if(conta.statusConta === 'PENDENTE') {
+            tr.setAttribute("class", "table-warning");
+        }
+
+        if(conta.atraso === true){
+            tr.setAttribute("class", "table-danger");
+        }
+
+        if(conta.statusConta === 'PAGO') {
+            tr.setAttribute("class", "table-success");
+        }
+
+        contaEmpresa.innerText = conta.contaEmpresa;
+        dtVenc.innerText = conta.dtVenc;
+        status.innerText = conta.statusConta;
+        valor.innerText = conta.valor;
+
+        tr.appendChild(contaEmpresa);
+        tr.appendChild(dtVenc);
+        tr.appendChild(status);
+        tr.appendChild(valor);
+
+        let imgRemove = document.createElement('img');
+        let imgEdit = document.createElement('img');
+        imgEdit.src = 'img/edit.png';
+        imgEdit.style = 'width: 45px'
+
+        imgRemove.src = 'img/icons8-x-16.png';
+        imgRemove.style = 'width: 45px'
+        imgRemove.setAttribute("onclick", )
+
+
+        tr.appendChild(imgEdit)
+        tr.appendChild(imgRemove)
+        
+
+
+        tbody.appendChild(tr);
+
+        
+    })*/
+}
+
+getAll();
