c7036bd933c2a0a68cdeeb16f774b233c60401b9
JulianoBat <julianobatss@gmail.com>
2023-05-31 15:48:04 -0300

    Adição btn edit e delete (btn delete funcional)

--- Interface_contas/js/script.js
+++ Interface_contas/js/script.js
@@ -15,8 +15,6 @@ function momentoAtual() {
 formEl.addEventListener('submit', evento => {
     evento.preventDefault();
     const IdataVenc = document.querySelector(".dt-venc").value;
-    console.log(IdataVenc);
-    console.log(momentoAtual());
 
     const momentoA = momentoAtual();
     const formData = new FormData(formEl);
@@ -33,9 +31,6 @@ formEl.addEventListener('submit', evento => {
         }
     }
 
-
-    console.log(verificaVenc());
-
     fetch('http://localhost:8080/conta', {
         method: 'POST',
         headers: {
@@ -45,3 +40,34 @@ formEl.addEventListener('submit', evento => {
     }).then(res => res.json()).then(data => console.log(data))
 });
 
+function deletar(i){
+    let confirmacao = confirm("Deletar conta cadastrada? ")
+    if(confirmacao){
+        const element = document.querySelector('#linha'+ i);
+        fetch('http://localhost:8080/conta/'+ i, { method: 'DELETE' })
+        .then(() => alert('Conta deletada com sucesso!'));
+        window.location.reload(true);
+    }
+}
+
+function alterar(i){
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: 'Fetch PUT Request Example' })
+    };
+    fetch('http://localhost:8080/conta/'+ i, requestOptions)
+        .then(response => response.json())
+        .then(data => element.innerHTML = data.updatedAt );
+}
+
+function preparaAlterar(dados){
+    document.getElementById('empresas').value = dados.contaEmpresa
+    document.getElementById('valor').value = dados.valor;
+    document.getElementById('dt-venc').value = dados.dtVenc;
+    document.getElementById('dt-pag').value = dados.dtPag;
+    document.getElementById('dt-emi').value = dados.dtEmi;
+
+    document.getElementById('btn1').innerText = "Alterar"
+    document.getElementById('offcanvasNavbarDarkLabel').innerText = "Altere uma conta"
+}
--- Interface_contas/js/scriptTabela.js
+++ Interface_contas/js/scriptTabela.js
@@ -6,15 +6,13 @@ async function getAll() {
     const response = await fetch(url)
     const data = await response.json();
     
-    
-    
     for(let i = 0; i < data.length; i++) {
         const tr = document.createElement("tr");
         const contaEmpresa = document.createElement("td");
         const dtVenc = document.createElement("td");
         const valor = document.createElement("td");
         const status = document.createElement("td");
-        
+        const id = data[i].id;
 
         contaEmpresa.innerText = data[i].contaEmpresa;
         dtVenc.innerText = data[i].dtVenc;
@@ -22,11 +20,31 @@ async function getAll() {
         status.innerText = data[i].statusConta;
         
 
+        tr.setAttribute("id", "linha" + id)
         tr.appendChild(contaEmpresa);
         tr.appendChild(dtVenc);
         tr.appendChild(valor);
         tr.appendChild(status);
         
+        valor.classList.add('center');
+
+        const imgEdit = document.createElement('img');
+        imgEdit.src = 'img/edit.png';
+        imgEdit.setAttribute("onclick", "preparaAlterar("+ JSON.stringify(data[i]) +")");
+        imgEdit.setAttribute("class", "navbar-toggler")
+        imgEdit.setAttribute("type", "button")
+        imgEdit.setAttribute("data-bs-toggle", "offcanvas")
+        imgEdit.setAttribute("data-bs-target", "#offcanvasNavbarDark")
+        imgEdit.setAttribute("aria-controls", "offcanvasNavbarDark")
+        imgEdit.setAttribute("aria-label", "Toggle navigation")
+
+        const imgDelete = document.createElement('img');
+        imgDelete.src = 'img/delete.png';
+        
+        imgDelete.setAttribute("onclick", "deletar("+ id +")")
+        imgDelete.setAttribute("type", "button")
+        tr.appendChild(imgEdit);
+        tr.appendChild(imgDelete);
 
         if(data[i].atraso === false && data[i].statusConta === 'PENDENTE') {
             tr.setAttribute("class", "table-warning");
@@ -38,8 +56,6 @@ async function getAll() {
         if(data[i].statusConta === 'PAGO') {
             tr.setAttribute("class", "table-success");
         }
-
-        
 
         tbody.appendChild(tr);
     }
