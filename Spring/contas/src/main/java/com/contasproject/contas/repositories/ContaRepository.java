c1797e8c23bd21e994936cd633b35a402e293dd0
JulianoBat <julianobatss@gmail.com>
2025-12-02 12:14:01 -0300

    Front-end refeito e organização dos arquivos

--- Spring/contas/src/main/java/com/contasproject/contas/repositories/ContaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.contasproject.contas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.contasproject.contas.entities.Conta;
-
-public interface ContaRepository extends JpaRepository<Conta, Long>{
-
-}
